8eaba3634e48f8fb9d5df8f2ab3e0ba332b28d97
phatlv <phatlv96@gmail.com>
2023-07-26 15:01:03 +0700

    Update Cors Config

--- server/src/configs/cors.config.js
+++ server/src/configs/cors.config.js
@@ -1,3 +1,6 @@
 exports.cors_config = {
-    origin: ['http://localhost:3000', 'https://a-little-todo-app-by-phat.netlify.app']
+    origin: [
+        'http://localhost:3000',
+        'https://a-little-todo-app-by-phat.netlify.app',
+        'https://demo-task-management-client.onrender.com/']
 }
